6a9380b4571783fed935d65313562440d1b377c3
Arnoke0508 <arnoke@msn.com>
2020-04-15 23:30:18 +0200

    init stash

---
+++ DatingAppApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DatingAppApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Value> Values { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options){}
+
+
+    }
+}
