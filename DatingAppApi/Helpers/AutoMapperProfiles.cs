9879bfb161012be54d19490b3cd900187b271e37
Arnoke0508 <arnoke@msn.com>
2020-05-09 23:22:11 +0200

    end of section 12

--- DatingAppApi/Helpers/AutoMapperProfiles.cs
+++ DatingAppApi/Helpers/AutoMapperProfiles.cs
@@ -21,6 +21,7 @@ namespace DatingAppApi.Helpers
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
